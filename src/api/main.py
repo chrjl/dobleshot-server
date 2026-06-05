4da31aaf86609d588f31c3cf25094344a5854c4a
chrjl <73675826+chrjl@users.noreply.github.com>
2026-06-05 12:34:45 -0700

    build(server): sqlalchemy engine in graphql context

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,22 @@
+from os import getenv
+from sqlalchemy.orm import sessionmaker
 from ariadne import gql, load_schema_from_path, make_executable_schema, QueryType
 from ariadne.asgi import GraphQL
+from ariadne.types import ContextValue
 
+from db.main import engine
+
+environment = getenv("APP_ENV")
 type_defs = gql(load_schema_from_path("src/schema/"))
+
+
+def get_context_value(request, _) -> ContextValue:
+    Session = sessionmaker(engine, autoflush=True)
+
+    return {
+        "request": request,
+        "Session": Session,
+    }
 
 
 query = QueryType()
@@ -14,4 +29,8 @@ def resolve_hello(*_):
 
 schema = make_executable_schema(type_defs, query)
 
-app = GraphQL(schema)
+app = GraphQL(
+    schema,
+    context_value=get_context_value,
+    debug=(environment == "development"),
+)
--- src/server/main.py
+++ src/server/main.py
@@ -3,29 +3,42 @@ from dotenv import load_dotenv
 import uvicorn
 
 
-def main():
-    APP_ENV = os.getenv("APP_ENV", "development")
+def main(options={}):
+    if ("host" not in options) and (host := os.getenv("UVICORN_HOST")):
+        options["host"] = host
+    if ("port" not in options) and (port := os.getenv("UVICORN_PORT")):
+        options["port"] = int(port)
 
-    if APP_ENV == "development":
-        return dev()
-    else:
-        return serve(APP_ENV)
+    uvicorn.run("server:app", **options)
 
 
-def serve(APP_ENV):
-    load_dotenv(f".env.{APP_ENV}")
-    uvicorn.run("server:app")
+def serve():
+    load_dotenv(f".env.{os.getenv("APP_ENV")}")
+    main()
 
 
 def dev():
+    os.environ["APP_ENV"] = "development"
+
     if os.path.exists(".env.development"):
         load_dotenv(".env.development")
     else:
         load_dotenv(".env")
 
-    uvicorn.run(
-        "server:app", reload=True, reload_includes="*.graphql", log_level="trace"
-    )
+    options = {
+        "reload": True,
+        "reload_includes": "*.graphql",
+        "log_level": "trace",
+    }
+
+    main(options)
+
+
+def test(host="127.0.0.1", port=8001):
+    os.environ["APP_ENV"] = "testing"
+
+    options = {"host": host, "port": port}
+    main(options)
 
 
 if __name__ == "__main__":
